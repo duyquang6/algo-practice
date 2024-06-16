28a9c36fdfac5ffcd86a0ab18b7abc8327927adc
duyquang6 <nguyenduyquang06@gmail.com>
2024-06-16 19:35:28 +0700

    add max heap

--- heap/heap.py
+++ heap/heap.py
@@ -18,7 +18,7 @@ def recur(arr, i):
     return True
 
 
-class Heap:
+class MinHeap:
     def __init__(self, arr=[]):
         self.heapify(arr)
         self.arr = arr
@@ -46,8 +46,56 @@ class Heap:
         recur(self.arr, 0)
 
 
+class MaxHeap:
+    def __init__(self, arr=[]):
+        for i in range(len(arr)):
+            arr[i] = -arr[i]
+        self.heapify(arr)
+        self.arr = arr
+
+    def heapify(self, arr):
+        n = len(arr)
+
+        for i in range(n):
+            recur(arr, i)
+
+    def pop(self):
+        arr = self.arr
+        n = len(arr)
+        if n == 0:
+            return None
+        val = arr[0]
+        arr[0], arr[n - 1] = arr[n - 1], arr[0]
+        arr.pop()
+        recur(arr, 0)
+
+        return -val
+
+    def push(self, val):
+        self.arr.append(-val)
+        recur(self.arr, 0)
+
+
 h = [100, 41, 51, 41, 31, 16, 13]
-heap = Heap(h)
+heap = MinHeap(h)
+print(h)
+
+pop_val = heap.pop()
+print("pop_val", pop_val)
+print(h)
+
+pop_val = heap.pop()
+print("pop_val", pop_val)
+print(h)
+
+heap.push(10)
+print("after_push", h)
+
+
+print("-------------Max Heap--------------")
+
+h = [100, 41, 51, 41, 31, 16, 13]
+heap = MaxHeap(h)
 print(h)
 
 pop_val = heap.pop()
